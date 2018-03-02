0749dd3976fb1575842af9e70256c39d78f89233
Phillip Shannon <shannonphillipl@gmail.com>
2018-03-02 00:01:50 +0000

    Fix an issue.

--- basicEnroll/Program.cs
+++ basicEnroll/Program.cs
@@ -16,8 +16,6 @@ namespace basicEnroll
         {
         
         
-        
-            int testVariable;
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
