69f820769db9badd8a1195c7cf2cdeeff52aba71
Phillip Shannon <shannonphillipl@gmail.com>
2018-03-04 20:24:46 +0000

    Program.cs edited online with Bitbucket

--- basicEnroll/Program.cs
+++ basicEnroll/Program.cs
@@ -9,7 +9,7 @@ namespace basicEnroll
     static class Program
     {
 	var unusedVariable;
-        var anotherUnused;
+        var anotherUnused = "update?";
         /// <summary>
 	///
         /// The main entry point for the application.
